00b5095e0d9f5d387cc01dfffa2b77cd200aeae5
Peter Arentsen <peter.arentsen@bettyblocks.com>
2025-09-22 16:55:03 +0200

    chore: bump version and elixir/erlang

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FileStorageApi.MixProject do
   def project do
     [
       app: :file_storage_api,
-      version: "2.2.0",
+      version: "2.3.0",
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       elixirc_options: [warnings_as_errors: Mix.env() != :test],
